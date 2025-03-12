d0aa120580688455c1bdf1b3d24727aa3027c1dd
Lucy Knowles <26lknowles@mtbluersd.org>
2025-03-11 22:13:13 -0400

    Lots of changes, mostly competition changes, vision, autos, and fixing constants from the encoders

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -15,6 +15,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -123,5 +124,7 @@ public class WristSubsystem extends SubsystemBase {
     if(RobotState.isDisabled()) {
       wristSetPosition = getWristPosition();
     }
+    SmartDashboard.putNumber(" Wrist Set Position", wristSetPosition);
+    SmartDashboard.putNumber("Real Position", wristMotor.getPosition().getValueAsDouble());
   }
 }
